c86638ba8ecf0b732875ce5b7862686979520387
zodaland <ekwhwhek@gmail.com>
2021-06-17 18:33:22 +0900

    [controller] 방 생성 함수 Promise -> async 함수로 전체 수정

--- api/room/controller.js
+++ api/room/controller.js
@@ -1,39 +1,36 @@
 const room = require('../../models/room')
 const Error = require('../../library/error')
+const Result = require('../../library/result')
 
-exports.create = (req, res) => {
+exports.create = async (req, res) => {
 	const body = req.body
-
 	const isPrivate = body.isPrivate || 0
-	console.log(isPrivate)
-	const checkValue = () => {
-		return new Promise((resolve, reject) => {
-			if (isPrivate !== 0 && isPrivate !== 1) {
-				console.log('asd')
-				reject('INVALID_VALUE')
-			}
-			resolve(isPrivate)
-		})
+
+	if (isPrivate !== 0 && isPrivate !== 1) {
+		res.json(Result.get('9999'))
+		return
 	}
 
-	const respond = (result) => {
-		res.json({
-			code: '0000',
-			data: result.insertId
-		})
-	}
+	try {
+		const unixEpoch = Math.floor(new Date().getTime() / 1000)
+		const params = [unixEpoch, isPrivate]
+		const returnData = await room.getConnection(params)
+			.then(room.create)
+		const data = { roomNo: returnData.insertId }
 
-	const respondError = (error) => {
-		res
-		.status(409)
-		.json(Error.get(error))
+		res.json(Result.get('0000', data))
+	} catch(error) {
+		const message = error.message
+		const code = '4000'
+
+		if (message.length === 4) {
+			code = message
+		} else {
+			console.log(error)
+		}
+
+		res.json(Result.get(code))
 	}
-	
-	checkValue()
-	.then(room.getConnection)
-	.then(room.create)
-	.then(respond)
-	.catch(respondError)
 }
 
 exports.find = (req, res) => {
